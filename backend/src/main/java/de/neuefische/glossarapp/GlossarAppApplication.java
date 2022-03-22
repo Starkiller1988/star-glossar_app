318ece40f94b0a3e8f693d8835da3a946cb4c3e9
Samir Schabel <90188949+Starkiller1988@users.noreply.github.com>
2022-03-22 12:16:49 +0100

    first commit

---
+++ backend/src/main/java/de/neuefische/glossarapp/GlossarAppApplication.java
@@ -0,0 +1,13 @@
+package de.neuefische.glossarapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlossarAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GlossarAppApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/de/neuefische/glossarapp/GlossarAppApplicationTests.java
@@ -0,0 +1,13 @@
+package de.neuefische.glossarapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlossarAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
